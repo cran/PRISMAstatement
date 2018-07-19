7b999dccfc9e8301ef53a39691c1ea281f94b87c
Jack O. Wasey <jack@jackwasey.com>
2018-07-19 03:50:02 +0000

    version 1.0.2

--- inst/doc/PRISMA.R
+++ inst/doc/PRISMA.R
@@ -16,3 +16,41 @@ prisma(found = 750,
        quantitative = 319,
        width = 800, height = 800)
 
+## ----prismadpi1, fig.cap="just set width and height"---------------------
+prisma(found = 750,
+       found_other = 123,
+       no_dupes = 776, 
+       screened = 776, 
+       screen_exclusions = 13, 
+       full_text = 763,
+       full_text_exclusions = 17, 
+       qualitative = 746, 
+       quantitative = 319,
+       width = 200, height = 200)
+
+## ----prismadpi2, fig.cap="same width and height but DPI increased to 300"----
+prisma(found = 750,
+       found_other = 123,
+       no_dupes = 776, 
+       screened = 776, 
+       screen_exclusions = 13, 
+       full_text = 763,
+       full_text_exclusions = 17, 
+       qualitative = 746, 
+       quantitative = 319,
+       width = 200, height = 200,
+       dpi = 300)
+
+## ----prismadpi3, fig.cap="same width and height but DPI decreased to 36"----
+prisma(found = 750,
+       found_other = 123,
+       no_dupes = 776, 
+       screened = 776, 
+       screen_exclusions = 13, 
+       full_text = 763,
+       full_text_exclusions = 17, 
+       qualitative = 746, 
+       quantitative = 319,
+       width = 200, height = 200,
+       dpi = 36)
+
